1f2b7b50fd11fef57f4eeb1a53548e6c810a540d
vhumenny <v.humenny@gmail.com>
2023-01-03 22:11:46 +0200

    parseCommandLine made changes. Parts of code are moved to a separate methods.

--- src/main/java/runner/FileModeRunner.java
+++ src/main/java/runner/FileModeRunner.java
@@ -88,29 +88,33 @@ public class FileModeRunner implements ModeRunner {
         String[] lineArray = line.toLowerCase().split(" ");
 
         switch (lineArray[0]) {
-            case "add" -> {
-                if (lineArray.length < 3) {
-                    System.out.println("Please enter the correct quantity of Product.");
-                    return;
-                }
-                int quantityProductsNeeded = Integer.parseInt(line.replaceAll("\\D", ""));
-                if (checkProductName(cart.getStorageMap(), lineArray[1]) && quantityProductsNeeded != 0) {
-                    cart.add(lineArray[1], quantityProductsNeeded);
-                } else System.out.println("Please enter the correct quantity and name of Product. " +
-                        "For example - 5 and bear");
-            }
+            case "add" -> addProductFromLine(line, cart, lineArray);
             case "price" -> cart.price();
-            case "discount" -> {
-                if (lineArray[1].equals("buy_3_get_1_free") && checkProductName(cart.getStorageMap(), lineArray[2]))
-                    cart.applyDiscount(new Discount_BUY_3_GET_1_FREE(), lineArray[2]);
-                else if (lineArray[1].equals("buy_1_get_30_percentage") &&
-                        checkProductName(cart.getStorageMap(), lineArray[2]))
-                    cart.applyDiscount(new Discount_BUY_1_GET_30_PERCENT_OFF(), lineArray[2]);
-                else System.out.println("You entered the wrong type of discount. Try next command, for example," +
-                            "\"discount buy_3_get_1_free soap\"");
-            }
+            case "discount" -> addDiscountFromLine(cart, lineArray);
             default -> System.out.println("неизвесная команда, попробуйте еще раз, например \"add bear 5\"");
         }
+    }
+
+    private static void addDiscountFromLine(Cart cart, String[] lineArray) {
+        if (lineArray[1].equals("buy_3_get_1_free") && checkProductName(cart.getStorageMap(), lineArray[2]))
+            cart.applyDiscount(new Discount_BUY_3_GET_1_FREE(), lineArray[2]);
+        else if (lineArray[1].equals("buy_1_get_30_percentage") &&
+                checkProductName(cart.getStorageMap(), lineArray[2]))
+            cart.applyDiscount(new Discount_BUY_1_GET_30_PERCENT_OFF(), lineArray[2]);
+        else System.out.println("You entered the wrong type of discount. Try next command, for example," +
+                    "\"discount buy_3_get_1_free soap\"");
+    }
+
+    private static void addProductFromLine(String line, Cart cart, String[] lineArray) {
+        if (lineArray.length < 3) {
+            System.out.println("Please enter the quantity of Product. For example - 5");
+            return;
+        }
+        int quantityProductsNeeded = Integer.parseInt(line.replaceAll("\\D", ""));
+        if (checkProductName(cart.getStorageMap(), lineArray[1]) && quantityProductsNeeded != 0) {
+            cart.add(lineArray[1], quantityProductsNeeded);
+        } else System.out.println("Please enter the correct quantity and name of Product. " +
+                "For example - 5 and bear");
     }
 
     private static boolean checkProductName(Map<String, Product> cart, String productName) {
