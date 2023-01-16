03bb3f911f43e7c2c7921493bb71fafb1cf257a0
KorbutViacheslav <minowaman777@gmail.com>
2023-01-16 10:06:13 +0200

    Fix finish command.And 5 corrected.

--- src/main/java/storage/StorageWithJson.java
+++ src/main/java/storage/StorageWithJson.java
@@ -33,10 +33,9 @@ public class StorageWithJson implements Storage {
     @Override
     public Map<String, Product> load(String file) {
         File jsonFile = new File(path);
-        List<Product> productList;
         Map<String, Product> productMap = new LinkedHashMap<>();
         try {
-            productList = objectMapper.readValue(jsonFile, new TypeReference<>() {
+            List<Product> productList = objectMapper.readValue(jsonFile, new TypeReference<>() {
             });
             productList.forEach(product -> productMap.put(product.getName(), product));
         } catch (IOException exception) {
