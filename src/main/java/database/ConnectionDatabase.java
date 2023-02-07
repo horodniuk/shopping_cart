3dc18977e2f7797b6a1b30d599475d8a908450b5
KorbutViacheslav <minowaman777@gmail.com>
2023-02-07 13:33:42 +0200

    Select to database.

--- src/main/java/database/ConnectionDatabase.java
+++ src/main/java/database/ConnectionDatabase.java
@@ -2,13 +2,32 @@ package database;
 
 import database.util.Connector;
 
-import java.sql.DriverManager;
+import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionDatabase {
     public static void main(String[] args) throws SQLException {
-        try (var connection = Connector.open()) {
-            System.out.println(connection.getTransactionIsolation());
+        String sql="""
+                SELECT *
+                FROM company_storage.products
+
+        """;
+
+        try (var connection = Connector.open();
+             var statement = connection.createStatement()) {
+
+            var executeResult = statement.executeQuery(sql);
+            Map<String,BigDecimal> list=new HashMap<>();
+            while (executeResult.next()){
+                System.out.print(executeResult.getString("name")+" price: ");
+                System.out.print(executeResult.getBigDecimal("price")+" quantity: ");
+                System.out.print(executeResult.getInt("quantity"));
+                System.out.println("");
+                list.put(executeResult.getString("name"),
+                        executeResult.getBigDecimal("price") );
+            }
         }
     }
 }
