0e773b4b94348aed77c5343c40be36bec73b8b7b
vhumenny <v.humenny@gmail.com>
2023-02-08 19:07:41 +0200

    refactored methods parse in ConsoleCommandParser nad ConfigReader. also refactored method start in DataSourceManager

--- src/main/java/ConfigReader.java
+++ src/main/java/ConfigReader.java
@@ -14,36 +14,51 @@ public class ConfigReader {
     /**
      * Method description
      * This methode is made to parse data from file of properties;
-     * First we announce variable dbType which is optional of DBType;
+     * we check if stream equals null;
+     * if true - we throw IllegalArgumentException;
+     * if false we go next;
      * next in try catch scope we create inputStreamReader to read properties file;
-     * we check if reader is ready to be read;
      * after that we create instance of class Properties and with its help load all properties from properties file;
      * we create stringDBType assign it value of property DB_TYPE;
-     * next we check if value of stringDBType equals STORAGE_JSON;
-     * if true - we reassign new value to DBType - STORAGE_JSON;
-     * if false - we check next statement;
-     * we check if value of stringDBType equals STORAGE_DATABASE;
-     * if true - we reassign new value to DBType - STORAGE_JSON;
-     * if false - we output message to console - that wrong dbType is specified in properties file.
-     * return - Optional of DBType (Enum).
+     * next we start method getDBType();
+     * return - DBType (Enum).
      */
-    public Optional<DbType> parse() {
-        Optional<DbType> dbType = Optional.empty();
-        try (InputStreamReader reader = new InputStreamReader(stream)) {
-            if (!reader.ready()) {
-                throw new IllegalArgumentException("Couldn't read file! Wrong path in stream!");
+    public DbType parse() {
+        if (stream == null) {
+            throw new IllegalArgumentException("Couldn't read file! Wrong path in stream!");
+        } else {
+            try (InputStreamReader reader = new InputStreamReader(stream)) {
+                Properties properties = new Properties();
+                properties.load(reader);
+                String stringDBType = properties.getProperty("DB_TYPE");
+                return getDBType(stringDBType);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-            Properties properties = new Properties();
-            properties.load(reader);
-            String stringDBType = properties.getProperty("DB_TYPE");
-            for (DbType db : DbType.values()) {
-                if (stringDBType.toUpperCase().equals(db.toString())) {
-                    dbType = Optional.of(db);
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-        return dbType;
+    }
+
+    /**
+     * Method description
+     * This methode is made to get DBType (enum);
+     * we announce variable dbType which is optional of DBType;
+     * next we check if value of stringDBType equals any DbType.values();
+     * if any equals - we reassign value of dbType to Optional of this DB (enum);
+     * after that we check if Optional of DBType is empty;
+     * if true we throw IllegalArgumentException;
+     * else - we return  Optional of DBType (Enum).
+     */
+    private DbType getDBType(String stringDBType) {
+        Optional<DbType> dbType = Optional.empty();
+        for (DbType db : DbType.values()) {
+            if (stringDBType.toUpperCase().equals(db.toString())) {
+                dbType = Optional.of(db);
+            }
+        }
+        if (dbType.isEmpty()) {
+            throw new IllegalArgumentException("Wrong DB type specified in file app.properties!");
+        } else {
+            return dbType.get();
+        }
     }
 }
--- src/main/java/DataSourceManager.java
+++ src/main/java/DataSourceManager.java
@@ -1,4 +1,3 @@
-import java.util.Optional;
 
 public class DataSourceManager {
     ConfigReader configReader;
@@ -10,21 +9,16 @@ public class DataSourceManager {
     /**
      * Method description
      * This methode is made to start app depending on db type specified in properties file;
-     * We create variable dbType which is optional of Class DBType and assign it value with method - parse();
-     * First we check if optional of DBType is empty.
-     * if true - we throw IllegalArgumentException;
-     * if false - next in switch case, we start AppByJsonStorage.start() or AppByDataBaseStorage.start() depending on
+     * We create variable dbType and assign it value with method - parse();
+     * next in switch case, we start AppByJsonStorage.start() or AppByDataBaseStorage.start() depending on
      * value of DBType;
      */
     void start() {
-        Optional<DbType> dbType = configReader.parse();
-        if (dbType.isEmpty()) {
-            throw new IllegalArgumentException("Wrong DB type specified in file app.properties!");
-        } else {
-            switch (dbType.get()) {
-                case STORAGE_JSON -> AppByJsonStorage.start();
-                case STORAGE_DATABASE -> AppByDataBaseStorage.start();
-            }
+        DbType dbType = configReader.parse();
+        switch (dbType) {
+            case STORAGE_JSON -> AppByJsonStorage.start();
+            case STORAGE_DATABASE -> AppByDataBaseStorage.start();
         }
     }
 }
+
