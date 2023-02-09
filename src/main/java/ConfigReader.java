a4b3b2043633040370a0efe060f94b3b5980e69a
vhumenny <v.humenny@gmail.com>
2023-02-09 18:29:50 +0200

    refactored method parseConfig() in ConfigReader. created class PropertyUtils with method readProperties()

--- src/main/java/ConfigReader.java
+++ src/main/java/ConfigReader.java
@@ -1,33 +1,18 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+
 
 public class ConfigReader {
     private final static String CORE_CONFIGURATION_DB_TYPE = "DB_TYPE";
-    private Properties properties;
 
-
-    public ConfigReader(String fileName) {
-        properties = readProperties(fileName);
-    }
-
-    private Properties readProperties(String fileName) {
-        Properties props = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
-        if (inputStream == null) {
-            throw new RuntimeException("Could not find property file: [" + fileName + "].");
-        }
-        try {
-            props.load(inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not read properties from file: [" + fileName + "].", e);
-        }
-        return props;
-    }
-
+    /**
+     * Method description
+     * This methode is made to parse data from file of properties;
+     * we return DBType (enum) by getting it constant PROPERTIES using String CORE_CONFIGURATION_DB_TYPE;
+     * if we are not able to get it from properties - we throw IllegalArgumentException
+     * ("Wrong DB type specified in file app.properties!");
+     */
     public DbType parseConfig() {
         try {
-            return DbType.valueOf(properties.getProperty(CORE_CONFIGURATION_DB_TYPE));
+            return DbType.valueOf(PropertyUtils.PROPERTIES.getProperty(CORE_CONFIGURATION_DB_TYPE));
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("Wrong DB type specified in file app.properties!");
         }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ConfigReader configReader = new ConfigReader("app.properties");
+        ConfigReader configReader = new ConfigReader();
         DataSourceManager dataSourceManager = new DataSourceManager(configReader);
         dataSourceManager.start();
     }
