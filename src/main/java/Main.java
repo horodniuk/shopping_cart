86d0df6f15488d7e7ffa0fc4fec71793fc73c129
horodniuk <horodniuk.maksym55@gmail.com>
2023-02-18 12:51:52 +0200

    add logback

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 import config.ConfigReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        LOGGER.info("Test work logger");
         ConfigReader configReader = new ConfigReader();
         DataSourceManager dataSourceManager = new DataSourceManager(configReader);
         dataSourceManager.start();
