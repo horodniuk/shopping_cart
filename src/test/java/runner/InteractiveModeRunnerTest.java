e4c3e34d27c68a5f1e145c46f410c80c7d6fa227
KorbutViacheslav <minowaman777@gmail.com>
2023-02-23 14:31:58 +0200

    Add test to InteractiveModeRunner.

--- src/test/java/runner/InteractiveModeRunnerTest.java
+++ src/test/java/runner/InteractiveModeRunnerTest.java
@@ -1,8 +1,53 @@
 package runner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import cart.Cart;
+import cart.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import storage.Storage;
+import storage.StorageWithJson;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class InteractiveModeRunnerTest {
+    InteractiveModeRunner interactiveModeRunner;
+    File path;
+    Storage storage;
 
+    @BeforeEach
+    void beforeEachTestMethod() throws URISyntaxException {
+        path = new File(getClass().getClassLoader().getResource("test_storage.json").toURI());
+        storage = new StorageWithJson(path);
+        interactiveModeRunner = new InteractiveModeRunner(storage);
+    }
+
+    @Test
+    void startExpectedProductBeer() {
+        String expected = "beer";
+        Product product = storage.getProductByName(expected);
+        String actual = product.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void startExpectedProductCola() {
+        String expected = "cola";
+        Product product = storage.getProductByName(expected);
+        String actual = product.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test()
+    void textCommandExecutorVerify() {
+        Cart cart = mock(Cart.class);
+        TextCommandExecutor textCommandExecutor = mock(TextCommandExecutor.class);
+        textCommandExecutor.executeCommand("finish", cart);
+        verify(textCommandExecutor).executeCommand("finish", cart);
+    }
 
 }
